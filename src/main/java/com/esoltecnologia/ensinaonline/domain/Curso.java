c2d6f47f22f9d300e37ce004807bad9f2e709afc
João Lima <jmflima@gmail.com>
2020-06-10 16:54:45 -0300

    Criado o dominio Curso

---
+++ src/main/java/com/esoltecnologia/ensinaonline/domain/Curso.java
@@ -0,0 +1,76 @@
+package com.esoltecnologia.ensinaonline.domain;
+
+import java.io.Serializable;
+
+public class Curso implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private Integer horas;
+	
+	public Curso() {
+		
+	}
+	
+	public Curso(Integer id, String nome, Integer horas) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.horas = horas;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getHoras() {
+		return horas;
+	}
+
+	public void setHoras(Integer horas) {
+		this.horas = horas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
--- src/main/java/com/esoltecnologia/ensinaonline/resouces/CursoResource.java
+++ src/main/java/com/esoltecnologia/ensinaonline/resouces/CursoResource.java
@@ -1,16 +1,31 @@
 package com.esoltecnologia.ensinaonline.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.esoltecnologia.ensinaonline.domain.Curso;
 
 @RestController
 @RequestMapping(value = "/cursos")
 public class CursoResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O Rest está funcionando!";
+	public List<Curso> listar() {
+		
+		Curso c1 = new Curso(1, "Spring Boot", 60);
+		Curso c2 = new Curso(2, "Java", 90);
+		Curso c3 = new Curso(3, "Rest", 40);
+		
+		List<Curso> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		lista.add(c3);
+		
+		return lista;
 	}
 
 }
